f4ff569aca92cbf70cfbf236b81d288b96bdd3db
Alexander Schranz <alexander@sulu.io>
2022-11-27 16:13:39 +0100

    Update rule to handle fluent items

--- rector/PHPArrayConfigToConfigBuilderRule.php
+++ rector/PHPArrayConfigToConfigBuilderRule.php
@@ -3,15 +3,18 @@
 namespace Rector\Symfony\Rector\ClassMethod;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\Array_;
 use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\Expr\MethodCall;
 use PHPStan\Type\ClassStringType;
 use PHPStan\Type\ObjectType;
 use Rector\Core\Rector\AbstractRector;
+use Rector\Core\Util\StringUtils;
 use Rector\PhpDocParser\NodeTraverser\SimpleCallableNodeTraverser;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+use function Symfony\Component\String\u;
 
 class PHPArrayConfigToConfigBuilderRule extends AbstractRector
 {
@@ -36,7 +39,7 @@ class PHPArrayConfigToConfigBuilderRule extends AbstractRector
     {
         $configClasses = [];
 
-        $this->simpleCallableNodeTraverser->traverseNodesWithCallable($node, function(Node $node) use (&$configClasses): ?array {
+        $this->simpleCallableNodeTraverser->traverseNodesWithCallable($node, function(Node $node) use (&$configClasses) {
             if (! $node instanceof MethodCall) {
                 return null;
             }
@@ -61,9 +64,9 @@ class PHPArrayConfigToConfigBuilderRule extends AbstractRector
             /** @var Node\Expr\Array_ $configArray */
             $configArray = $node->args[1]->value;
 
-            $stmts = $this->transformItems(
+            $stmts = $this->transformFluentItems(
                 $configVariable,
-                $configArray->items
+                $configArray
             );
 
             return $stmts; // FIXME fails here: "System error: "enterNode() returned invalid value of type array"
@@ -80,19 +83,56 @@ class PHPArrayConfigToConfigBuilderRule extends AbstractRector
     /**
      * @return Node\Expr[]
      */
-    private function transformItems($configVariable, array $items): array
+    private function transformFluentItems(MethodCall|string $methodCallOrVariableName, Array_ $items): array
     {
         $stmts = [];
 
-        foreach ($items as $item) {
-            $stmts[] = $this->nodeFactory->createMethodCall(
-                $configVariable,
-                $item->key->value,
-                [$item->value]
+        $currentStmt = null;
+        foreach ($items->items as $item) {
+            $method = $this->underscoreToCamelCase($item->key->value);
+            $value = $item->value;
+
+            if ($item->value instanceof Array_) {
+                // TODO array node does not automatically tell us that a fluent call is possible
+                //      some key -> value like like twig globals need some handing here
+                //      or example an array node like here: https://github.com/sulu/sulu/blob/7562cff8c54df40a81527b6ff717c22674a5c1b3/src/Sulu/Bundle/MediaBundle/DependencyInjection/Configuration.php#L92-L98
+                $fluentStmts = $this->transformFluentItems(
+                    $currentStmt ?? $this->nodeFactory->createMethodCall($methodCallOrVariableName, $method), $value
+                );
+
+                $currentStmt = array_pop($fluentStmts);
+
+                foreach ($fluentStmts as $stmt) {
+                    $fluentStmts[] = $stmt;
+                }
+
+                continue;
+            }
+
+            if ($currentStmt) {
+                $stmts[] = $currentStmt;
+            }
+
+            $currentStmt = $this->nodeFactory->createMethodCall(
+                $currentStmt ?? $methodCallOrVariableName,
+                $method,
+                [$value]
             );
         }
 
+        if ($currentStmt) {
+            $stmts[] = $currentStmt;
+        }
+
         return $stmts;
+    }
+
+    private function underscoreToCamelCase(string $name): string
+    {
+        $uppercaseWords = \ucwords($name, '_');
+        $pascalCaseName = \str_replace('_', '', $uppercaseWords);
+
+        return \lcfirst($pascalCaseName);
     }
 
     /**
